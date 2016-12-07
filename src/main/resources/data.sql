57961bcee4e9084c070fc44a8a040b3fc9be588c
j4 <j4st.j4h@gmail.com>
2016-12-07 11:58:31 +0200

    fixed all issues with fetching data except loading only part of object added new StatusUpdates rules added new dependencies to pom.xml (rest-mvc, rest-data) added data and schema.sql (not actually good one)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ insert INTO issueTracker.task (name, description, creation_date, modification_da
 VALUES ('1 Task' , 'First Task', 1481103343, 1481103343 , 1, 'TODO', 1, 1);
 
 insert INTO issueTracker.task (name, description, creation_date, modification_date, project_id, status, creator_id, assignee_id)
-VALUES ('2 Task' , 'Second Task', 1481103343, 1481103343 , 1, 'TODO', 1, 2);
+VALUES ('2 Task' , 'Second Task', 1481103343, 1481103343 , 2, 'TODO', 1, 2);
 
 insert INTO issueTracker.task (name, description, creation_date, modification_date, project_id, status, creator_id, assignee_id)
 VALUES ('3 Task' , 'Third One', 1481103343, 1481103343 , 1, 'INPROGRESS', 1, 1);
@@ -22,4 +22,8 @@ VALUES ('2 SubTask' , 'Second ST', 1481103343, 1481103343 , 3, 'INPROGRESS');
 INSERT INTO issueTracker.user (nickname, name, surname, email, password) VALUES
  ('kriva', 'Andrew', 'Krivonosov', 'ak47@levi9.com', '$2a$08$Wd09nbSNrsu1/e1gqYzpiO1S6QXkZEzGqDGP0G6PzooKLcoTp7EV2');
 
+ INSERT INTO issueTracker.user (nickname, name, surname, email, password) VALUES
+ ('j4', 'Arnold', 'Khovrashkov', 'j4h@leprosorium.ru', '$2a$08$Wd09nbSNrsu1/e1gqYzpiO1S6QXkZEzGqDGP0G6PzooKLcoTp7EV2');
+
 INSERT INTO issueTracker.project_user (project_id, user_id) VALUES (1, 1);
+INSERT INTO issueTracker.project_user (project_id, user_id) VALUES (2, 2);
